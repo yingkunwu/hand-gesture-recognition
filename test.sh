e5f69959d3da8b6d28280bb0c1d2129ea162cbb8
Ying-Kun Wu <wu001210@ahl04.agate.msi.umn.edu>
2022-11-04 18:16:55 -0500

    add executable files

---
+++ test.sh
@@ -0,0 +1,6 @@
+python test.py  \
+        --data_folder hagrid/hagrid_test \
+        --model_name CPM.model \
+        --batch_size 2 \
+        --img_size 368 \
+        --sigma 3
---
+++ train.sh
@@ -0,0 +1,8 @@
+python train.py \
+        --data_folder hagrid/hagrid_test \
+        --model_name CPM.model \
+        --epochs 2 \
+        --lr 0.000004 \
+        --batch_size 16 \
+        --img_size 368 \
+        --sigma 3
