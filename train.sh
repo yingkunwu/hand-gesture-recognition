983b7154a0660a2f820728dceecebe57ec08a185
kunnnnethan <ethanwu1127@gmail.com>
2022-11-09 17:28:15 -0600

    remove .sh files

--- train.sh
+++
@@ -1,8 +0,0 @@
-python train.py \
-        --data_folder hagrid/hagrid_test \
-        --model_name CPM.model \
-        --epochs 2 \
-        --lr 0.000004 \
-        --batch_size 16 \
-        --img_size 368 \
-        --sigma 3
