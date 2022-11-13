db45259ddc457e2ad1d741718e00785b746ae3e0
kunnnnethan <ethanwu1127@gmail.com>
2022-11-13 14:57:42 -0600

    modify model to also output classification

--- model/posenet.py
+++ model/posenet.py
@@ -1,84 +1,8 @@
 import torch
 from torch import nn
 
-
-class Bottleneck(nn.Module):
-    expansion = 4
-
-    def __init__(self, inplanes, planes, stride=1, downsample=None, bn_momentum=0.1):
-        super(Bottleneck, self).__init__()
-        self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(planes, momentum=bn_momentum)
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(planes, momentum=bn_momentum)
-        self.conv3 = nn.Conv2d(planes, planes * self.expansion, kernel_size=1, bias=False)
-        self.bn3 = nn.BatchNorm2d(planes * self.expansion, momentum=bn_momentum)
-        self.relu = nn.ReLU(inplace=True)
-        self.downsample = downsample
-        self.stride = stride
-
-    def forward(self, x):
-        residual = x
-
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
-
-        out = self.conv2(out)
-        out = self.bn2(out)
-        out = self.relu(out)
-
-        out = self.conv3(out)
-        out = self.bn3(out)
-
-        if self.downsample is not None:
-            residual = self.downsample(x)
-
-        out += residual
-        out = self.relu(out)
-
-        return out
-
-
-class BasicBlock(nn.Module):
-    expansion = 1
-
-    def __init__(self, inplanes, planes, stride=1, downsample=None, bn_momentum=0.1):
-        super(BasicBlock, self).__init__()
-        self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(planes, momentum=bn_momentum)
-        self.relu = nn.ReLU(inplace=True)
-        self.conv2 = nn.Conv2d(inplanes, planes, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(planes, momentum=bn_momentum)
-        self.downsample = downsample
-        self.stride = stride
-
-    def forward(self, x):
-        residual = x
-
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
-
-        out = self.conv2(out)
-        out = self.bn2(out)
-
-        if self.downsample is not None:
-            residual = self.downsample(x)
-
-        out += residual
-        out = self.relu(out)
-
-        return out
-
-
-resnet_spec = {
-    18: (BasicBlock, [2, 2, 2, 2]),
-    34: (BasicBlock, [3, 4, 6, 3]),
-    50: (Bottleneck, [3, 4, 6, 3]),
-    101: (Bottleneck, [3, 4, 23, 3]),
-    152: (Bottleneck, [3, 8, 36, 3])
-}
+from model.module import resnet_spec
+from model.cbam import cbam
 
 
 # derived from https://github.com/leoxiaobin/deep-high-resolution-net.pytorch
@@ -102,19 +26,38 @@ class PoseResNet(nn.Module):
 
         # used for deconv layers
         self.deconv_with_bias = False
-        self.deconv_layers = self._make_deconv_layer(
+        self.deconv_layers1 = self._make_deconv_layer(
             3,
             [256, 256, 256],
             [4, 4, 4],
             bn_momentum=bn_momentum
         )
 
-        self.final_layer = nn.Conv2d(
-            in_channels=256,
+        self.deconv_layers2 = self._make_deconv_layer(
+            3,
+            [256, 256, 256],
+            [4, 4, 4],
+            bn_momentum=bn_momentum
+        )
+
+        self.attantion_layer1 = cbam(512)
+        self.attantion_layer2 = cbam(512)
+
+        self.heatmap_layer = nn.Conv2d(
+            in_channels=512,
             out_channels=nof_joints,
             kernel_size=1,
             stride=1,
             padding=0
+        )
+
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+
+        self.classification_layer = nn.Sequential(
+            nn.Linear(512, 512),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(512, 10)
         )
 
     def _make_layer(self, block, planes, blocks, stride=1, bn_momentum=0.1):
@@ -185,7 +128,26 @@ class PoseResNet(nn.Module):
         x = self.layer3(x)
         x = self.layer4(x)
 
-        x = self.deconv_layers(x)
-        x = self.final_layer(x)
+        y1 = self.deconv_layers1(x)
+        y2 = self.deconv_layers1(x)
 
-        return x
+        y = torch.cat([y1, y2], dim=1)
+
+        y1 = self.attantion_layer1(y)
+        y1 = y + y1
+        heatmap = self.heatmap_layer(y1)
+
+        y2 = self.attantion_layer2(y)
+        y2 = y + y2
+        y2 = self.avgpool(y2)
+        y2 = y2.view(y2.size(0), -1)
+        label = self.classification_layer(y2)
+
+        return heatmap, label
+
+
+if __name__ == "__main__":
+    x = torch.randn((8, 3, 256, 256))
+    module = PoseResNet()
+    y1, y2 = module(x)
+    print(y1.shape, y2.shape)
